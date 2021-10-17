f1ac2b34ac7d57fa115f33e437c11d67331e5551
Shetu Twinbit PC <shetu.twinbit@gmail.com>
2021-10-17 18:59:16 +0600

    z exclude

--- app/Actions/GenerateKeyFromIdAction.php
+++ app/Actions/GenerateKeyFromIdAction.php
@@ -9,37 +9,37 @@ class GenerateKeyFromIdAction
             if ($id < 16777216) {
                 $string_length = strlen(strtoupper(dechex($id)));
                 if ($string_length == 2) {
-                    $key = substr(str_shuffle(str_repeat('ADGJMPSVY', mt_rand(1,3))), 1, 3).$id;
+                    $key = substr(str_shuffle(str_repeat('ADGJMPSVY', mt_rand(1,4))), 1, 4).$id;
                 } elseif ($string_length == 3) {
-                    $key = substr(str_shuffle(str_repeat('ADGJMPSVY', mt_rand(1,2))), 1, 2).$id;
+                    $key = substr(str_shuffle(str_repeat('ADGJMPSVY', mt_rand(1,3))), 1, 3).$id;
                 } elseif ($string_length == 4) {
-                    $key = substr(str_shuffle(str_repeat('ADGJMPSVY', mt_rand(1,1))), 1, 1).$id;
+                    $key = substr(str_shuffle(str_repeat('ADGJMPSVY', mt_rand(1,2))), 1, 2).$id;
                 } elseif ($string_length == 5) {
-                    $key = 'Z'.$id;
+                    $key = substr(str_shuffle(str_repeat('ADGJMPSVY', mt_rand(1,1))), 1, 1).$id;
                 }
             } elseif ($id >= 16777216 && $id < 33554432) {
                 $id = $id - 16777215;
                 $string_length = strlen(strtoupper(dechex($id)));
                 if ($string_length == 2) {
-                    $key = substr(str_shuffle(str_repeat('BEHKNQTWZ', mt_rand(1,3))), 1, 3).$id;
+                    $key = substr(str_shuffle(str_repeat('BEHKNQTWZ', mt_rand(1,4))), 1, 4).$id;
                 } elseif ($string_length == 3) {
-                    $key = substr(str_shuffle(str_repeat('BEHKNQTWZ', mt_rand(1,2))), 1, 2).$id;
+                    $key = substr(str_shuffle(str_repeat('BEHKNQTWZ', mt_rand(1,3))), 1, 3).$id;
                 } elseif ($string_length == 4) {
-                    $key = substr(str_shuffle(str_repeat('BEHKNQTWZ', mt_rand(1,1))), 1, 1).$id;
+                    $key = substr(str_shuffle(str_repeat('BEHKNQTWZ', mt_rand(1,2))), 1, 2).$id;
                 } elseif ($string_length == 5) {
-                    $key = 'Z'.$id;
+                    $key = substr(str_shuffle(str_repeat('BEHKNQTWZ', mt_rand(1,1))), 1, 1).$id;
                 }
             } else {
                 $id = $id - 33554431;
                 $string_length = strlen(strtoupper(dechex($id)));
                 if ($string_length == 2) {
-                    $key = substr(str_shuffle(str_repeat('CFILORUX', mt_rand(1,3))), 1, 3).$id;
+                    $key = substr(str_shuffle(str_repeat('CFILORUX', mt_rand(1,4))), 1, 4).$id;
                 } elseif ($string_length == 3) {
-                    $key = substr(str_shuffle(str_repeat('CFILORUX', mt_rand(1,2))), 1, 2).$id;
+                    $key = substr(str_shuffle(str_repeat('CFILORUX', mt_rand(1,3))), 1, 3).$id;
                 } elseif ($string_length == 4) {
-                    $key = substr(str_shuffle(str_repeat('CFILORUX', mt_rand(1,1))), 1, 1).$id;
+                    $key = substr(str_shuffle(str_repeat('CFILORUX', mt_rand(1,2))), 1, 2).$id;
                 } elseif ($string_length == 5) {
-                    $key = 'Z'.$id;
+                    $key = substr(str_shuffle(str_repeat('CFILORUX', mt_rand(1,1))), 1, 1).$id;
                 }
             }
             return $key;
